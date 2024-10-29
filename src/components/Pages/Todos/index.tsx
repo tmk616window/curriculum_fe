222a7368fd8a86366e5bb2eef2b24fe42f5afa0e
tmk616window <tmk616window@gmail.com>
2024-10-29 12:24:19 +0900

    query params

--- src/components/Pages/Todos/index.tsx
+++ src/components/Pages/Todos/index.tsx
@@ -3,7 +3,10 @@ import TodoList from '@/components/feature/todoList/todoList'
 
 const TodoContent: React.FC = ({
 }) => {
-  const { data, error, isLoading } = useGetTodos()
+  const { data, error, isLoading } = useGetTodos({
+    limit: 10,
+    offset: 0
+  })
 
   if (isLoading) {
     return <div>Loading...</div>
