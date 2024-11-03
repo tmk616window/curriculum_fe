023c897ea24f908f773a43c45950084023262dea
tmk616window <tmk616window@gmail.com>
2024-11-03 17:06:19 +0900

    zod 修正

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,9 +1,9 @@
-import { TypeOf, ZodSchema } from "zod";
+import { z, ZodSchema } from "zod";
 import { UseFormReturn, useForm as useReactHookForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-export const useForm = <T extends ZodSchema>(schema: T): UseFormReturn<TypeOf<T>> => {
-  return useReactHookForm<TypeOf<T>>({
+export const useForm = <T extends ZodSchema>(schema: T): UseFormReturn<z.infer<T>> => {
+  return useReactHookForm<z.infer<T>>({
     resolver: zodResolver(schema),
   });
 };
