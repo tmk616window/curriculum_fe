21a73201a134a7f8d0869fc8c18e20f68b79ff7b
tmk616window <tmk616window@gmail.com>
2024-11-05 01:15:39 +0900

    ページネーション 整備

--- src/components/feature/todoList/todoList.tsx
+++ src/components/feature/todoList/todoList.tsx
@@ -1,70 +1,100 @@
 import { Todo } from '@/api/generated/todoAppAPI.schemas'
-import { IconButton, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow, TextField } from '@mui/material'
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material'
 import { useState } from 'react'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import DoneIcon from '@mui/icons-material/Done';
+import { getTodosQueryParams } from '@/api/generated/zod/todoAppAPI';
+import { TypeOf } from 'zod';
+import { QueryKey, useQueryClient } from '@tanstack/react-query';
 
 type TodoListProps = {
   todoList: Todo[]
+  setSearch: (search: TypeOf<typeof getTodosQueryParams>) => void
+  search: TypeOf<typeof getTodosQueryParams>
+  queryKey: QueryKey
 }
 
 const TodoList: React.FC<TodoListProps> = ({
-  todoList
+  todoList,
+  setSearch,
+  search,
+  queryKey
 }) => {
+  const queryClient = useQueryClient()
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setSearch({ offset: parseInt(event.target.value, 5) })
+    queryClient.invalidateQueries({ queryKey })
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper} sx={{ maxHeight: 350 }}>
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{ paddingY: 0.5 }}>タイトル</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}>ステータス</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}>優先度</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}>作成日</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}>完了日</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}>説明</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}>ラベル</TableCell>
-            <TableCell sx={{ paddingY: 0.5 }}></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {todoList.map((todo) => (
-            <TableRow key={todo.id} sx={{ paddingY: 0.5 }}>
-              <TableCell sx={{ paddingY: 0.5 }}>{todo.title}</TableCell>
-              <TableCell sx={{ paddingY: 0.5 }}>{todo.status.value}</TableCell>
-              <TableCell sx={{ paddingY: 0.5 }}>{todo.priority.name}</TableCell>
-              <TableCell sx={{ paddingY: 0.5 }}>{new Date(todo.createdAt).toLocaleDateString()}</TableCell>
-              <TableCell sx={{ paddingY: 0.5 }}>
-                {todo.finishedAt ? new Date(todo.finishedAt).toLocaleDateString() : '-'}
-              </TableCell>
-              <TableCell sx={{ paddingY: 0.5 }}>{todo.description || '-'}</TableCell>
-              <TableCell sx={{ paddingY: 0.5 }}>
-                {todo.labels ? todo.labels.map(label => label.value).join(', ') : '-'}
-              </TableCell>
-              <TableCell sx={{ paddingY: 0.5, display: 'flex', gap: 1 }}>
-                <IconButton color="success">
-                  <DoneIcon />
-                </IconButton>
-                <IconButton color="primary">
-                  <EditIcon />
-                </IconButton>
-                <IconButton color="secondary">
-                  <DeleteIcon />
-                </IconButton>
-              </TableCell>
+    <>
+      <TableContainer component={Paper} sx={{ maxHeight: 300 }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ paddingY: 0.5 }}>タイトル</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}>ステータス</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}>優先度</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}>作成日</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}>完了日</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}>説明</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}>ラベル</TableCell>
+              <TableCell sx={{ paddingY: 0.5 }}></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      {/* <TablePagination
-        component="div"
-        count={100}
+          </TableHead>
+          <TableBody>
+            {todoList.map((todo) => (
+              <TableRow key={todo.id} sx={{ paddingY: 0.5 }}>
+                <TableCell sx={{ paddingY: 0.5 }}>{todo.title}</TableCell>
+                <TableCell sx={{ paddingY: 0.5 }}>{todo.status.value}</TableCell>
+                <TableCell sx={{ paddingY: 0.5 }}>{todo.priority.name}</TableCell>
+                <TableCell sx={{ paddingY: 0.5 }}>{new Date(todo.createdAt).toLocaleDateString()}</TableCell>
+                <TableCell sx={{ paddingY: 0.5 }}>
+                  {todo.finishedAt ? new Date(todo.finishedAt).toLocaleDateString() : '-'}
+                </TableCell>
+                <TableCell sx={{ paddingY: 0.5 }}>{todo.description || '-'}</TableCell>
+                <TableCell sx={{ paddingY: 0.5 }}>
+                  {todo.labels ? todo.labels.map(label => label.value).join(', ') : '-'}
+                </TableCell>
+                <TableCell sx={{ paddingY: 0.5, display: 'flex', gap: 1 }}>
+                  <IconButton color="success">
+                    <DoneIcon />
+                  </IconButton>
+                  <IconButton color="primary">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton color="secondary">
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        rowsPerPage={rowsPerPage}
+        count={todoList.length}
         page={page}
         onPageChange={handleChangePage}
-        rowsPerPage={rowsPerPage}
         onRowsPerPageChange={handleChangeRowsPerPage}
-      /> */}
-    </TableContainer>
+      />
+    </>
   )
 }
 
