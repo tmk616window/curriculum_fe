71eea9a524cceca63be3a85f10691edf2fa06b6d
tmk616window <tmk616window@gmail.com>
2024-11-04 19:26:33 +0900

    クエリパラメータ変換

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -3,13 +3,17 @@
 
 import Axios, { AxiosError, AxiosRequestConfig } from 'axios'
 import { API_BASE_URL } from '@/constants/env'
+import qs from 'qs';
 
 export const AXIOS_INSTANCE = Axios.create({ baseURL: API_BASE_URL ? API_BASE_URL : 'http://localhost:8001', timeout: 30000 })
 
 export const useCustomInstance = <T>(): ((config: AxiosRequestConfig) => Promise<T>) => {
   return async (config: AxiosRequestConfig) => {
-    const promise = AXIOS_INSTANCE({
+    const promise = AXIOS_INSTANCE.request({
       ...config,
+      paramsSerializer: (params) => {
+        return qs.stringify(params, { arrayFormat: 'repeat' });
+      },
     }).then(({ data }) => data)
 
     return promise
